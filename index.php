5d369d6e4bfcadc253d42ad637cc17dab7873949
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-06 00:36:47 +0330

    Implement a progress bar

--- index.php
+++ index.php
@@ -18,22 +18,32 @@
                     </div>
                     <button class="btn btn-danger">submit</button>
                 </form>
+                <div class="mt-4 d-none" id="progress">
+                    <div class="progress">
+                        <div class="progress-bar" style="width: 0%">0%</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
     <script>
         const form = document.querySelector('#upload');
-       
+        const progress = document.getElementById('progress');
+        const progressBar = progress.querySelector('.progress-bar');
+
         form.addEventListener('submit' , function(event) {
             event.preventDefault();
-      
+            
             let file = this.querySelector(`#fileupload`).files[0]
             if(file) {
+                progress.classList.remove('d-none');
+
                 let formdata = new FormData();
                 formdata.append('file' ,file);
 
                 let ajax = new XMLHttpRequest();
+                ajax.upload.addEventListener('progress' , progressHandler)
                 ajax.addEventListener('load' , completeHandler)
 
                 ajax.open('POST' , '/upload.php');
@@ -41,6 +51,11 @@
             }
         })
 
+        function progressHandler(event) {
+            let percent = Math.round(( event.loaded / event.total ) * 100);
+            progressBar.style.width = `${percent}%`
+            progressBar.innerHTML = `${percent}%`
+        }
 
         function completeHandler() {
             console.log('complete')
